055fd0ca80d45493109cace6e85e0b5007e74aa0
vinayak-narayankar-au8 <nlsvinayak@gmail.com>
2020-08-05 14:07:44 +0530

    Adding from feature branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./App.css";
 function App() {
-  return <div className="App">Hi vinayak </div>;
+  return <div className="App">Feature 1 changing</div>;
 }
 
 export default App;
