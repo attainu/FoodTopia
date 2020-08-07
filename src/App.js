c0861a5bc24dbee673b0641dd56d68a5d19828e0
Parth Vaidhya <pv.parth96@gmail.com>
2020-08-07 09:38:27 +0530

    Basic Navbar and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar.js'
+//import Home from './pages/Home'
+import Footer from './components/Footer/Footer'
 
-import "./App.css";
-function App() {
-  return <div className="App">Hi vinayak </div>;
+
+// cuma buat test aja
+// import Test from './components/Testing/Test'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                
+                <Footer />
+            </div>
+        );
+    }
+
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+// import React, { Component } from 'react'
+// import SearchCity from '../components/SearchCity'
+
+// class HOME extends Components {
+
+// }
+
+
+// export default HOME;
