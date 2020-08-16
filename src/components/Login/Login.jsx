8d84029374e5bc4445ac783ede601afc609644ab
Parth Vaidhya <pv.parth96@gmail.com>
2020-08-16 23:29:38 +0530

    Styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,12 +3,49 @@ import React, { Component } from "react";
 import loginImg from "../../assets/food.jpg";
 import "../Login/Login.css";
 //import Responsive from 'react-responsive-decorator';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
+const CLIENT_ID = '<your Client ID>';
 
 class Login extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isLogined: false,
+      accessToken: ''
+    };
+
+    this.login = this.login.bind(this);
+    this.handleLoginFailure = this.handleLoginFailure.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleLogoutFailure = this.handleLogoutFailure.bind(this);
   }
+
+  login (response) {
+    if(response.accessToken){
+      this.setState(state => ({
+        isLogined: true,
+        accessToken: response.accessToken
+      }));
+    }
+  }
+
+  logout (response) {
+    this.setState(state => ({
+      isLogined: false,
+      accessToken: ''
+    }));
+  }
+
+  handleLoginFailure (response) {
+    alert('Failed to log in')
+  }
+
+  handleLogoutFailure (response) {
+    alert('Failed to log out')
+  }
+
 
   render() {
     return (
@@ -18,13 +55,29 @@ class Login extends Component {
           <div className="image">
             <img src={loginImg} />
           </div>
+
+          <div className="google-btn">
+      { this.state.isLogined ?
+        <GoogleLogout
+          clientId={ CLIENT_ID }
+          buttonText='Sign-out'
+          onLogoutSuccess={ this.logout }
+          onFailure={ this.handleLogoutFailure }
+        >
+        </GoogleLogout>: <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText='Sign-in with Google'
+          onSuccess={ this.login }
+          onFailure={ this.handleLoginFailure }
+          cookiePolicy={ 'single_host_origin' }
+          responseType='code,token'
+        />
+      }
+      { this.state.accessToken ? <h5>Your Access Token: <br/><br/> { this.state.accessToken }</h5> : null }
+
+    </div>
        
-          <div className="google-btn" >
-            <button id="loginButton">
-            <i class="fa fa-google" id="icon" ></i>
-              Sign-In with Google
-            </button>
-          </div>
+          
 
           <div className="form">
             <div className="form-group">
